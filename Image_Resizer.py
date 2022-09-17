6b8d265ddb5efefdd46c03b0433f415dabae252f
이진영 <jakerhee321@gmail.com>
2022-09-17 13:26:03 +0900

    Add image and code

--- Image_Resizer.py
+++ Image_Resizer.py
@@ -1,1 +1,7 @@
 from PIL import Image
+
+image = Image.open("image_before.png")
+print(f"Current size : {image.size}")
+
+resized_image = image.resize((500,500))
+resized_image.save("image_after.png")
