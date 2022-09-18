c9326c2aa765f237cb137b6afc0773c7a24b08d2
이진영 <jakerhee321@gmail.com>
2022-09-18 10:50:07 +0900

    Added User Input

--- Image_Resizer.py
+++ Image_Resizer.py
@@ -1,7 +1,14 @@
 from PIL import Image
 
-image = Image.open("image_before.png")
-print(f"Current size : {image.size}")
+def resize_image(size1, size2):
 
-resized_image = image.resize((500,500))
-resized_image.save("image_after.png")
+    image = Image.open("image_before.png")
+    print(f"Current size : {image.size}")
+
+    resized_image = image.resize((size1, size2))
+    resized_image.save("image_after"+ str(size1) + 'x' + str(size2) + ".png")
+
+
+size1 = int(input("Width: "))
+size2 = int(input("Length: "))
+resize_image(size1, size2)
